bae56d582a109832ede4e3b3ae043726271981c2
ruben compres <ruben compres>
2016-05-11 18:31:53 -0500

    changes to game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,7 @@ var guesses= 0;
 var letterToGuess= "";
 var lettersAlreadyGuessed= "";
 
-alert("guess right or lose a life.  Guess A-Z")
+alert("guess right or lose a life.  Guess A-Z");
 
 
 
@@ -14,21 +14,35 @@ alert("guess right or lose a life.  Guess A-Z")
 var computerChoices=['q','w','e','r','t','y','u','i','o','p','a','s','d','f'
 ,'g','h','j','k','l','z','x','c','v','b','n','m'];
 
-var arrayLength= computerChoices.length
+var arrayLength= computerChoices.length;
 
 // function gameStart() {
-	for (i=0; i<26; i++){
-		console.log([comSelects] +1)
+	{
+		console.log([comSelects] +1);
 	
 
 
 		var comSelects = Math.floor(Math.random() * computerChoices.length);
-		lettertoGuess = comSelects(computerChoices);
-		lettersAlreadyGuessed = [""];
+		lettertoGuess = computerChoices[comSelects];
+		lettersAlreadyGuessed =" ";
 		gameOver = false;
 		document.getElementById("lettersGuessed").innerHTML = lettersAlreadyGuessed;
 
 	
+
+
+}
+
+document.onkeyup =function(event) {
+	var userLetter =String.fromCharCode(event.keyCode).toLowerCase();
+
+if (userLetter== letterToGuess){
+	alert ("you win");
+}
+else{	
+	lettersAlreadyGuessed += userLetter
+	document.getElementbyId("lettersGuessed").innerHTML= lettersAlreadyGuessed;
+}
 
 
 }
